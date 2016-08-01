c6d0bd19fe35bd92ee4c2529536ccd24562f5418
Simon Eric Scholl <simon@sdscholl.de>
2016-08-01 17:01:24 +0200

    add missing licence annotation

--- app/code/local/SScholl/Onix/Model/Product/Processor/Abstract.php
+++ app/code/local/SScholl/Onix/Model/Product/Processor/Abstract.php
@@ -1,5 +1,14 @@
-<?php
-abstract class SScholl_Onix_Model_Product_Processor_Abstract
+<?php /**************** Copyright notice ************************
+ *  (c) 2011 Simon Eric Scholl <simon@sdscholl.de>
+ *  All rights reserved
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ ***************************************************************/
+
+class SScholl_Onix_Model_Product_Processor_Abstract
 {
 
 	private $_product	= null;
